60b77fdc1f1b842df73f1926a38474c7cb5a5e5c
Marsha Annon <marshaannon@gmail.com>
2020-03-10 22:28:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,51 +1,15 @@
 class Shoe
 
+  attr_reader :color, :size, :material, :condition
+  attr_accessor :brand
+
   def initialize(brand)
     @brand = brand
-  end
-
-  def brand
-    @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
   end
 
   def cobble
     puts "Your shoe is as good as new!"
     return @condition = "new"
   end
-
-
-
 
 end
